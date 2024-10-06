af5d711b4bae13e7cc0293481ab72dcea690c29a
Evie Lau <github@evie.ee>
2024-10-06 01:48:55 -0500

    Cleanup and formatting

--- checkSeason.py
+++ checkSeason.py
@@ -9,27 +9,27 @@ tvdb = tvdb_v4_official.TVDB(apikey)
 
 # TODO: iterate through PR changes of newly added seasons, and run the following for each show/season
 
-showName = "KamiErabi GOD.app"
-seasonNumber = 2
+showName = "Bananya"
+seasonNumber = 4
 foundSeason = False
 
-# Get ID of show
+# Get TVDB ID of show
 showId = None
 searchResults = tvdb.search(showName)
 for result in searchResults:
-    print(result['translations']['eng'])
     if ((showName == result['name']) or
         ('aliases' in result and showName in result['aliases']) or
         ('translations' in result and 'eng' in result['translations'] and showName == result['translations']['eng'])
         ):
         showId = result['tvdb_id']
+        break
 
 if (showId is None):
-    sys.exit("Did not find result for show title: " + showName)
+    sys.exit("Did not find tvdb result for show title: " + showName)
 else:
-    print("Found show: " + showName + " with id: " + showId)
+    print("Found show: " + showName + " with tvdb id: " + showId)
 
-# fetching a season's episode list
+# Check if official season number exists for the show in TVDB
 series = tvdb.get_series_extended(showId)
 for season in sorted(series["seasons"], key=lambda x: (x["type"]["type"], x["number"])):
     # print(season)
@@ -38,8 +38,11 @@ for season in sorted(series["seasons"], key=lambda x: (x["type"]["type"], x["num
         break
     else:
         season = None
+
 if season is not None:
     foundSeason = True
     print("Found season: " + str(seasonNumber))
 else:
     sys.exit("Did not find season: " + str(seasonNumber))
+
+# TODO: cross reference anilist-id show name
