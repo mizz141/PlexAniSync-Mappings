0c0b229c5aee67c2316bf4b3c90a076834754cce
Evie Lau <github@evie.ee>
2024-10-06 02:48:18 -0500

    Large refactor and modularize. Add ability to scan yaml file

--- checkSeason.py
+++ checkSeason.py
@@ -1,4 +1,4 @@
-import os, sys, tvdb_v4_official
+import os, sys, tvdb_v4_official, yaml
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -7,42 +7,63 @@ load_dotenv(Path(".env"))
 apikey = os.getenv("TVDB_API_KEY")
 tvdb = tvdb_v4_official.TVDB(apikey)
 
-# TODO: iterate through PR changes of newly added seasons, and run the following for each show/season
+def getTvdbId(showName):
+    # Get TVDB ID of show
+    showId = None
+    searchResults = tvdb.search(showName)
+    for result in searchResults:
+        if ((showName == result['name']) or
+            ('aliases' in result and showName in result['aliases']) or
+            ('translations' in result and 'eng' in result['translations'] and showName == result['translations']['eng'])
+            ) and result['primary_type'] == "series":
+            # print(result)
+            showId = result['tvdb_id']
+            break
+    return showId
 
-showName = "Bananya"
-seasonNumber = 4
-foundSeason = False
+def validateShowSeason(showName, seasonNumber):
+    foundSeason = False
+    hasError = False
 
-# Get TVDB ID of show
-showId = None
-searchResults = tvdb.search(showName)
-for result in searchResults:
-    if ((showName == result['name']) or
-        ('aliases' in result and showName in result['aliases']) or
-        ('translations' in result and 'eng' in result['translations'] and showName == result['translations']['eng'])
-        ):
-        showId = result['tvdb_id']
-        break
+    showId = getTvdbId(showName)
 
-if (showId is None):
-    sys.exit("Did not find tvdb result for show title: " + showName)
-else:
+    if (showId is None):
+        print("No TVDB series result: " + showName)
+        return
     print("Found show: " + showName + " with tvdb id: " + showId)
+    # Check if official season number exists for the show in TVDB
+    series = tvdb.get_series_extended(showId) # TODO: does not work for primary_type: movie, maybe separate method for those?
+    for season in sorted(series["seasons"], key=lambda x: (x["type"]["type"], x["number"])):
+        # print(season)
+        if season["type"]["type"] == "official" and season["number"] == seasonNumber:
+            season = tvdb.get_season_extended(season["id"])
+            break
+        else:
+            season = None
 
-# Check if official season number exists for the show in TVDB
-series = tvdb.get_series_extended(showId)
-for season in sorted(series["seasons"], key=lambda x: (x["type"]["type"], x["number"])):
-    # print(season)
-    if season["type"]["type"] == "official" and season["number"] == seasonNumber:
-        season = tvdb.get_season_extended(season["id"])
-        break
+    if season is not None:
+        foundSeason = True
+        print("Found season: " + str(seasonNumber))
     else:
-        season = None
+        hasError = True
+        print("Did not find season: " + str(seasonNumber))
 
-if season is not None:
-    foundSeason = True
-    print("Found season: " + str(seasonNumber))
-else:
-    sys.exit("Did not find season: " + str(seasonNumber))
+    if hasError:
+        # print("The following show to season mappings were not found on TVDB:")
+        # print(mismatchMap)
+        sys.exit(1)
+
+# TODO: get only PR changes of newly added seasons
+def validateAllMappings():
+    with open("custom_mappings.yaml") as f:
+        mappings = yaml.safe_load(f)
+        for show in sorted(mappings['entries'], key=lambda entry: (entry['title'], entry['seasons'])):
+            showName = show['title']
+            seasons = [s['season'] for s in show['seasons'] if 'season' in s]
+            # print(showName + ": " + str(seasons))
+            for season in seasons:
+                validateShowSeason(showName, season)
 
 # TODO: cross reference anilist-id show name
+validateShowSeason("The Heroic Legend of Arslan (2015)", 1)
+# validateAllMappings()
